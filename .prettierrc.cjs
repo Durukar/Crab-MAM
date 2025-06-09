8b22f60d46e6294cc9f2d06fd44da51f00f14964
Durukar <nightdavila@gmail.com>
2025-06-09 19:00:51 -0300

    build: First project configuration

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 75,
+  tabWidth: 2,
+  bracketSameLine: true,
+  endOfLine: 'lf',
+}
